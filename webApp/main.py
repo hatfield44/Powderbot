c7cf7a11360f22734423ae531cf00e118a764e90
hatfield44 <hatfield442000@yahoo.com>
2021-09-21 14:21:20 -0400

    Added forecast attribution

--- webApp/main.py
+++ webApp/main.py
@@ -1,3 +1,7 @@
+#Forecast information provided by 
+  #Open Weather  https://home.openweathermap.org/users/sign_up
+  #Weather API   https://www.weatherapi.com/signup.aspx
+  #Weatherbit    https://www.weatherbit.io/account/create
 # Uncomment scheduler lines if needed
 # Python anywhere required the use of their task scheduler as apscheduler is not supported
 from website import create_app
