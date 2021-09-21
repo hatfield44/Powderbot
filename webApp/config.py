a5fc32dd23f45626c45bdc2356ce05b44668c872
hatfield44 <hatfield442000@yahoo.com>
2021-09-21 14:46:25 -0400

    Create config.py

---
+++ webApp/config.py
@@ -0,0 +1,6 @@
+import secrets
+
+openWeatherAppID = "" # Enter your openweather App ID
+weatherAPIKey = "" # Enter your weatherapi.com API Key
+weatherBitKey = "" # Enter your weatherbit.io API Key
+secretKey = secrets.token_urlsafe(16)
