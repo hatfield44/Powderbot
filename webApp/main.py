7dc0f514e35798252a50b36812982717b4feb003
hatfield44 <hatfield442000@yahoo.com>
2021-09-21 13:53:15 -0400

    Add commented scheduler lines
    Add commented scheduler lines  for deployment outside of Python Anywhere

--- webApp/main.py
+++ webApp/main.py
@@ -1,8 +1,11 @@
+# Uncomment scheduler lines if needed
+# Python anywhere required the use of their task scheduler as apscheduler is not supported
 from website import create_app
-
+#from apscheduler.schedulers.background import BackgroundScheduler
 
 app = create_app()
-
+#scheduler = BackgroundScheduler()
 if __name__ == '__main__':
-
+    #scheduler.add_job(getForecast, trigger = 'cron', hour = 10)
+    #scheduler.start()
     app.run(use_reloader=False)
