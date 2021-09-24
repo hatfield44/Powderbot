ca3855f7989c2e207b4a58fcb397580f37e50bfc
hatfield44 <hatfield442000@yahoo.com>
2021-09-24 09:37:55 -0400

    Fixed day of date going past number of days in month

--- webApp/website/static/javascript/dateIncrement.js
+++ webApp/website/static/javascript/dateIncrement.js
@@ -4,6 +4,7 @@ n =  new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
+j =0;
 var leapYear; 
 document.getElementById("date").innerHTML = m + "/" + d + "/" + y; // todays date
 // leap year check
@@ -18,40 +19,47 @@ if (y%4 != 0){
  for (let i = 1; i < 11; i++){
    // increment day
    // even months with 30 days
-   if (m%2 == 0 && d < 30 && m != 2){
-       document.getElementById("date"+i).innerHTML = m + "/" + (d+i) + "/" + y;
+   if (m%2 == 0 && d+i <= 30 && m != 2){
+       document.getElementById("date"+i).innerHTML = m + "/" + (d+i-j) + "/" + y;
    // odd months with 31 days
-    } else if (m%2 == 1 && d < 31 && m != 2){
-       document.getElementById("date"+i).innerHTML = m + "/" + (d+i) + "/" + y;
+    } else if (m%2 == 1 && d+i <= 31 && m != 2){
+       document.getElementById("date"+i).innerHTML = m + "/" + (d+i-j) + "/" + y;
    // feb that odd bastard
-    } else if (m == 2 && d < 28 && leapYear == false){
-       document.getElementById("date"+i).innerHTML = m + "/" + (d+i) + "/" + y;
+    } else if (m == 2 && d+i <= 28 && leapYear == false){
+       document.getElementById("date"+i).innerHTML = m + "/" + (d+i-j) + "/" + y;
        // feb in leap year
-    } else if (m == 2 && d < 29 && leapYear == true){
-       document.getElementById("date"+i).innerHTML = m + "/" + (d+i) + "/" + y;
-    }
+    } else if (m == 2 && d+i <= 29 && leapYear == true){
+       document.getElementById("date"+i).innerHTML = m + "/" + (d+i-j) + "/" + y;
+    } 
+
     // increment month
-    if (m%2 == 0 && d == 30 && m != 2 && m < 12){
-       d = 1
-       m += 1
-       document.getElementById("date"+i).innerHTML = (m) + "/" + d + "/" + y;
-    } else if (m%2 == 1 && d == 31 && m < 12){
-       d = 1
-       m += 1
-       document.getElementById("date"+i).innerHTML = (m) + "/" + d + "/" + y;
-    } else if (m == 2 && d == 28 && leapYear == false){
-       d = 1
-       m += 1
-       document.getElementById("date"+i).innerHTML = (m) + "/" + d + "/" + y;
-    } else if ( m== 2 && d == 29 && leapYear == true){
-       d = 1
-       m += 1
-       document.getElementById("date"+i).innerHTML = (m) + "/" + d + "/" + y;
+    if (m%2 == 0 && d+i > 30 && m != 2 && m < 12){
+       j=i;
+       d = 1;
+       m += 1;
+       document.getElementById("date"+i).innerHTML = m + "/" + (d+i-j) + "/" + y;
+    } else if (m%2 == 1 && d+i > 31 && m < 12){
+       j=i;
+       d = 1;
+       m += 1;
+       document.getElementById("date"+i).innerHTML = m + "/" + (d+i-j) + "/" + y;
+    } else if (m == 2 && d+i > 28 && leapYear == false){
+       j=i;
+       d = 1;
+       m += 1;
+       document.getElementById("date"+i).innerHTML = m + "/" + (d+i-j) + "/" + y;
+    } else if ( m== 2 && d+i > 29 && leapYear == true){
+       j=i;
+       d = 1;
+       m += 1;
+       document.getElementById("date"+i).innerHTML = m + "/" + (d+i-j) + "/" + y;
     }
+
     // increment year
-    if (m == 12 && d == 31){
+    if (m == 12 && d+i > 31){
         m = 1
         d = 1
         y += 1
-    }
+        document.getElementById("date"+i).innerHTML = m + "/" + d + "/" + y;
+      }
  }
