5c0fc1256982341bbfbb33f1923a642f882a9a0e
hatfield44 <hatfield442000@yahoo.com>
2022-10-13 10:37:57 -0400

    Added Visual Crossing API as forecast source
    Got rid of hard coded number of locations
    Got rid of some commented artifacts

--- webApp/getForecasts.py
+++ webApp/getForecasts.py
@@ -3,7 +3,7 @@
 import requests
 import time
 from datetime import datetime
-from config import openWeatherAppID, weatherAPIKey, weatherBitKey
+from config import openWeatherAppID, weatherAPIKey, weatherBitKey, visualCrossingKey
 import sqlite3
 
 def test():
@@ -12,15 +12,28 @@ def test():
 def getForecast():
     print("\nGetting Forecast\n")
 
-    conn = sqlite3.connect('website/database.db')
+    conn = sqlite3.connect('/database.db')
     c = conn.cursor()
 
-    # Enter your credentials in the appropriate variable between the ""
-    #openWeatherAppID = config.openWeatherAppID # Enter your openweather App ID
-    #weatherAPIKey = config.weatherAPIKey # Enter your weatherapi.com API Key
-    #weatherBitKey = config.weatherBitKey # Enter your weatherbit.io API Key
+    # Gets forecast from Visual Crossing
+    def visualCrossingCall(lattitude, longitude, visualCrossingKey):
+        # Creates URL for call
+        url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=" + lattitude + "," + longitude + "&aggregateHours=24&unitGroup=us&shortColumnNames=false&contentType=json&key=" + visualCrossingKey
+        data = requests.get(url) # Performs call and gets data
+        forecast = data.json() # Process data to JSON
+        forecastDays = {} # Dictionary to hold each days forecast
 
+        # For each day of the forecast add the date and snowfall amount to forecastDays
+        for day in forecast["locations"][lattitude + "," + longitude]["values"]:
+            longDate = day["datetimeStr"] # Gets date and time
+            splitdate = longDate.split("T") # Splits date and time
+            date = splitdate[0]
+            date = date[5:7] + "/" + date[8:] + "/" + date[2:4] # Formats date
+            # deals with the last date being "none" instead of a number
+            if type(day["snow"]) == float:
+                forecastDays[date] = day["snow"]
 
+        return forecastDays # Returns dictonary of forecast by days
 
     # Gets forecast from Open Weather
     def openWeatherCall(lattitude, longitude, openWeatherAppID):
@@ -44,7 +57,7 @@ def getForecast():
     # Gets forecast from weather API
     def weatherAPICall(lattitude, longitude, weatherAPIKey):
         # URL for the API call
-        weatherAPIcomURL = "http://api.weatherapi.com/v1/forecast.json?key=8f62acbc4d494321a97221253200811&q=" + lattitude + "," + longitude + "&days=10"
+        weatherAPIcomURL = "http://api.weatherapi.com/v1/forecast.json?key=8f62acbc4d494321a97221253200811&days=3&q=" + lattitude + "," + longitude
         data = requests.post(weatherAPIcomURL) # Requests the data
         forecastData = data.json() # Processes data in JSON format
         forecastDays = {} # Dictonary to hold forecast to return data to main program
@@ -59,12 +72,13 @@ def getForecast():
                 forecastDays[date] = entry["day"]["totalprecip_in"]
             else:
                 forecastDays[date] = 0
+
         return forecastDays # Returns dictonary of forecast by days
 
 
     # Gets forecast from weather bit
     def weatherBitCall(lattitude, longitude, weatherBitKey):
-        weatherBitURL = "https://api.weatherbit.io/v2.0/forecast/daily?lat="+ lattitude + "&lon=" + longitude + "&days=10&units=I&key=" + weatherBitKey
+        weatherBitURL = "https://api.weatherbit.io/v2.0/forecast/daily?lat="+ lattitude + "&lon=" + longitude + "&days=15&units=I&key=" + weatherBitKey
         data = requests.post(weatherBitURL) # Requests the data
         forecast = data.json() # Processes data in JSON format
         forecastDays = {} # Dictonary to hold forecast to return data to main program
@@ -76,33 +90,38 @@ def getForecast():
             date = LongDate[5:7], "/", LongDate[8:10], "/", LongDate[2:4]
             date = "".join(date)
             forecastDays[date] = day["snow"]
+
         return forecastDays # Returns dictonary of forecast by days
 
     def forecastAggregation(lattitude, longitude):
         aggregateSnowfall = {} # Holds aggregate snowfall for each day while getting forecast
         aggregateForecast = {} # Holds aggregate forecast form each call
+        retrievedDays = 0 # Holds number of forecast days retrieved in case an API doesn't respond
 
+        # Try calling Visual Crossing and print error message if problem arises otherwise add days forecast to aggregateSnowfall
+        try:
+            visualCrossing = visualCrossingCall(lattitude, longitude, visualCrossingKey)
+        except:
+            print("visualCrossing Error")
+        else:
+            for date, forecast in visualCrossing.items():
+                aggregateSnowfall[date] = [forecast]
+                retrievedDays = 15
+
+        # Try calling WeatherBit and print error message if problem arises otherwise add days forecast to aggregateSnowfall
         try:
             weatherBit = weatherBitCall(lattitude, longitude, weatherBitKey)
         except:
             print("WeatherBit Error")
         else:
             for date, forecast in weatherBit.items():
-                aggregateSnowfall[date] = [forecast]
-            retrievedDays = 10
-
-        try:
-            weatherAPI = weatherAPICall(lattitude, longitude, weatherAPIKey)
-        except:
-            print("WeatherAPI Error")
-        else:
-            for date, forecast in weatherAPI.items():
                 try:
-                    aggregateSnowfall[date].append(forecast)
+                    aggregateSnowfall[date].append([forecast])
                 except:
                     aggregateSnowfall[date] = [forecast]
-                    retrievedDays = 8
+                    retrievedDays = 10
 
+        # Try calling OpenWeather and print error message if problem arises otherwise add days forecast to aggregateSnowfall
         try:
             openWeather = openWeatherCall(lattitude, longitude, openWeatherAppID)
         except:
@@ -113,9 +132,22 @@ def getForecast():
                     aggregateSnowfall[date].append(forecast)
                 except:
                     aggregateSnowfall[date] = [forecast]
+                    retrievedDays = 8
+
+        # Try calling WeatherAPI and print error message if problem arises otherwise add days forecast to aggregateSnowfall
+        try:
+            weatherAPI = weatherAPICall(lattitude, longitude, weatherAPIKey)
+        except:
+            print("Weather API Error")
+        else:
+            for date, forecast in weatherAPI.items():
+                try:
+                    aggregateSnowfall[date].append(forecast)
+                except:
+                    aggregateSnowfall[date] = [forecast]
                     retrievedDays = 3
 
-
+        # Gets the average from the snowfall totals for each date
         for date, prediction in aggregateSnowfall.items():
             aggregateForecast[date] = "{:.2f}".format(sum(aggregateSnowfall[date])/len(aggregateSnowfall[date]))
         aggregateForecast["days"] = retrievedDays
@@ -124,17 +156,19 @@ def getForecast():
 
 
     def dbForecast():
-        for i in range(1, 330):
+        c.execute("SELECT COUNT(*) FROM location")  # Gets number of locations from DB
+        mountainCount = c.fetchone()[0]  # Variable used to hold number of locations
+        for i in range(1, mountainCount + 1):
             for row in c.execute("SELECT name FROM location WHERE id =" + str(i)):
                 print(str(row)[2:-3])
             for row in c.execute("SELECT lat FROM location WHERE id =" + str(i)):
                 lattitude = str(row).replace("',)", "")
                 lattitude = lattitude.replace("('", "")
             for row in c.execute("SELECT lon FROM location WHERE id =" + str(i)):
-                longitutde = str(row).replace("',)", "")
-                longitutde = longitutde.replace ("('", "")
+                longitude = str(row).replace("',)", "")
+                longitude = longitude.replace ("('", "")
             # get forecast info
-            forecast = forecastAggregation(lattitude, longitutde)
+            forecast = forecastAggregation(lattitude, longitude)
             output = []
             for date, amount in forecast.items():
                 output.append(amount)
