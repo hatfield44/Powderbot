1e023182a8dae72943f7470340024c5b0c11fbb6
hatfield44 <hatfield442000@yahoo.com>
2022-10-13 10:39:31 -0400

    Addition of Visual Crossing

--- webApp/config.py
+++ webApp/config.py
@@ -3,4 +3,5 @@ import secrets
 openWeatherAppID = "" # Enter your openweather App ID
 weatherAPIKey = "" # Enter your weatherapi.com API Key
 weatherBitKey = "" # Enter your weatherbit.io API Key
+visualCrossingKey = "" # Enter you visualcrossing.com API Key
 secretKey = secrets.token_urlsafe(16)
