138e6164eb3b02a9bbfa80f95dcd09be7115170b
hatfield44 <hatfield442000@yahoo.com>
2022-10-13 10:45:33 -0400

    added attribution for visual crossing

--- webApp/main.py
+++ webApp/main.py
@@ -1,7 +1,8 @@
 #Forecast information provided by 
-  #Open Weather  https://home.openweathermap.org/users/sign_up
-  #Weather API   https://www.weatherapi.com/signup.aspx
-  #Weatherbit    https://www.weatherbit.io/account/create
+  #Open Weather     https://home.openweathermap.org/users/sign_up
+  #Weather API      https://www.weatherapi.com/signup.aspx
+  #Weatherbit       https://www.weatherbit.io/account/create
+  #Visual Crossing  https://www.visualcrossing.com/
 # Uncomment scheduler lines if needed
 # Python anywhere required the use of their task scheduler as apscheduler is not supported
 from website import create_app
